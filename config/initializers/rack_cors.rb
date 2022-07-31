300f02ab44193e2ff335876c6033a09cb99d418b
Malo Skrylevo <majioa@yandex.ru>
2022-07-31 21:29:54 +0300

    fix: add cors allow for put/patch
    ! cors allow for put/patch

--- config/initializers/rack_cors.rb
+++ config/initializers/rack_cors.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
    config.middleware.insert_before 0, Rack::Cors do
       allow do
          origins '*'
-         resource '*', headers: :any, methods: %i(get post options)
+         resource '*', headers: :any, methods: %i(get post put patch options)
       end
    end
 end if Rails.env.production?
