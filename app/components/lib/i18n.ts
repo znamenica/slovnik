d035ca04c5ee13bf32247165ce5c1d3e0ed9daa2
Malo Skrylevo <majioa@yandex.ru>
2022-08-12 00:10:31 +0300

    clean: removed all comiled forntend from the repo

--- app/components/lib/i18n.ts
+++
@@ -1,8 +0,0 @@
-import {createContext} from "react";
-
-export const I18nContext = createContext<any>({});
-
-export async function setLanguageAsync(language: string) {
-    const newDict = await import(`../locales/${language}.json`);
-    return newDict.default;
-}
