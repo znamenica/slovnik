f772adcd179f264dfac766def97cf36817495f04
Malo Skrylevo <majioa@yandex.ru>
2022-11-21 00:00:35 +0300

    fix: typo for redisize config
    ! typo for redisize config

--- config/initializers/redisize.rb
+++ config/initializers/redisize.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-Redisize.adapter_kind = :Inline # if Rails.env.production? || Rails.env.staging?
+Redisize.adapter_kind = :inline if Rails.env.production? || Rails.env.staging?
